1e341600ba131be292cc3c33f56506bee8e56406
Mirco Sair Salcedo <78182502+mirko2816@users.noreply.github.com>
2024-05-29 23:40:42 -0500

    Upload Program.cs
    1er problema resuelto nivel basico

---
+++ 1-Watermelon/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program 
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        if (n == 2)
+        {
+            Console.WriteLine("NO");
+        }
+        else if(n % 2 == 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else 
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
