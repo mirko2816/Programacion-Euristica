4f2e494bf10b148143b2be3b237bcf15377855f8
Mirco <mirkosair28@gmail.com>
2024-06-15 15:01:21 -0500

    Actualizado

--- 2-Next-round/obj/Debug/net8.0/2-Next-round.AssemblyInfo.cs
+++ 2-Next-round/obj/Debug/net8.0/2-Next-round.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2-Next-round")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d48386f9c78bb9a1e2fd9c0dc1c3f1c8f613b941")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9494ed7766ef5a286003af8b564f584a6b3411ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("2-Next-round")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2-Next-round")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
