b97043acdf44be01500db2ecd0573aadafeb4d7b
Mirco <mirkosair28@gmail.com>
2024-06-15 15:27:46 -0500

    Resuelto

--- 4-Boy-or-Girl/Program.cs
+++ 4-Boy-or-Girl/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string? nick = Console.ReadLine();
+        List<char> subnick = new List<char>();
+
+        foreach (char ch in nick)
+        {
+            if (!subnick.Contains(ch))
+            {
+                subnick.Add(ch);
+            }
+        }
+        
+        if(subnick.Count % 2 == 0)
+        {
+            Console.WriteLine("CHAT WITH HER!");
+        }else
+        {
+            Console.WriteLine("IGNORE HIM!");
+        }
+    }
+}
