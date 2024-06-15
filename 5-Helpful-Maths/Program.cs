f34f749a8a85db416a927950cc0a97c41d9de542
Mirco <mirkosair28@gmail.com>
2024-06-15 16:05:38 -0500

    Resuelto

--- 5-Helpful-Maths/Program.cs
+++ 5-Helpful-Maths/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string? input = Console.ReadLine();
+
+        List<int> ints = input
+                            .Where(c => char.IsDigit(c))    // Filtra solo los caracteres numéricos
+                            .Select(c => Convert.ToInt32(c.ToString())) // Convierte cada carácter a su valor entero
+                            .ToList();
+
+        ints.Sort();
+
+        string finalString = string.Join("+", ints);
+
+        Console.WriteLine(finalString);
+    }
+}
--- 5-Helpful-Maths/obj/Debug/net8.0/5-Helpful-Maths.AssemblyInfo.cs
+++ 5-Helpful-Maths/obj/Debug/net8.0/5-Helpful-Maths.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5-Helpful-Maths")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97043acdf44be01500db2ecd0573aadafeb4d7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc13d8ec0f78cffcd8d983d0badabcf6af751f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("5-Helpful-Maths")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5-Helpful-Maths")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
