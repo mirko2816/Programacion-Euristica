c2ec16abc8874de78f7ad7091a2b3162b7c2b477
Mirco <mirkosair28@gmail.com>
2024-06-21 16:05:14 -0500

    Resuelto

---
+++ 6-Queue-at-the-School/Program.cs
@@ -0,0 +1,35 @@
+﻿// https://codeforces.com/problemset/problem/266/B
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int n = nums[0];
+        int x = nums[1];
+
+        char[] fila = Console.ReadLine().ToCharArray();
+
+        if (n > 1)
+        {
+            // debe estar dentro de otro bucle que vaya de 0 a x
+            for (int j = 0; j < x; j++)
+            {
+                for(int i = 0; i < fila.Length-1; i++)
+                {
+                    if(fila[i] == 'B' && fila[i+1] == 'G')
+                    {
+                        fila[i] = 'G';
+                        fila[i+1] = 'B';
+                        i++; // para que el indice salte dos posiciones enves de una
+                    }
+                }
+            }
+
+            Console.WriteLine(fila);
+        }else
+        {
+            Console.WriteLine(fila);
+        }
+    }
+}
