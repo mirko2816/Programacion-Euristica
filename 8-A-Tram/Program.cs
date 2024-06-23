d0c6951060323bd5f00407bdca3f636af39cfd39
Mirco <mirkosair28@gmail.com>
2024-06-22 22:07:07 -0500

    Resuelto

---
+++ 8-A-Tram/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://codeforces.com/problemset/problem/116/A
+
+int n = int.Parse(Console.ReadLine());
+int total = 0;
+int totalMax = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int[] nums = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+    int salen = nums[0];
+    int entran = nums[1];
+
+    total = total - salen;
+    total = total + entran;
+
+    if (total > totalMax)
+    {
+        totalMax = total;
+    }
+}
+
+Console.WriteLine(totalMax);
+
